f24be0af2200c97f71e3e4571c26e0096a1c1bb0
Hakan Macat <hakan.macat@beyondsc.de>
2024-07-15 12:10:07 +0300

    added azure service bus task.

---
+++ 9. Azure Service Bus/Task/AzureServiceBusDemo/AzureServiceBusDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureServiceBusDemo;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var connectionString = configuration["ServiceBus:ConnectionString"];
+        var queueName = configuration["ServiceBus:QueueName"];
+
+        await using var client = new ServiceBusClient(connectionString);
+
+        ServiceBusSender sender = client.CreateSender(queueName);
+        await SendMessageAsync(sender);
+        await ReceiveMessagesAsync(client, queueName);
+
+        Console.ReadKey();
+    }
+
+    static async Task SendMessageAsync(ServiceBusSender sender)
+    {
+        var messageBody = $"Message with id: {Guid.NewGuid()}";
+        ServiceBusMessage message = new(messageBody);
+
+        await sender.SendMessageAsync(message);
+
+        Console.WriteLine($"Message sent. Message body:{messageBody}");
+    }
+
+    static async Task ReceiveMessagesAsync(ServiceBusClient client, string queueName)
+    {
+        ServiceBusProcessor processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync();
+    }
+
+    static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        Console.WriteLine($"Received message: {body}");
+        await args.CompleteMessageAsync(args.Message);
+    }
+
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine($"Exception occured:{args.Exception}");
+        return Task.CompletedTask;
+    }
+}
---
+++ 9. Azure Service Bus/Task/AzureServiceBusDemo/README.md
@@ -0,0 +1,2 @@
+For security issues, I didn't include the connection string in the appsetting.json file.
+You can try the app if you already have a test queue, or I can send you the connection string on your request.
