bc2070f43a8aa86b7d32781a2affdd62a6a5f1f6
Hakan Macat <hakan.macat@beyondsc.de>
2024-07-16 13:55:36 +0300

    added signalr

---
+++ 10. SignalR/Task/README.md
@@ -0,0 +1,4 @@
+App is implemented using the official Microsoft tutorial here:
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-8.0&tabs=visual-studio
+
+Once the application is started, you can open different browsers to test the application for multiple users.
---
+++ 10. SignalR/Task/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalRChat.Hubs;
+
+public class ChatHub : Hub
+{
+    // Hub method which receives the messages from a client and sends back to the receivers. 
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
